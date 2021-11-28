ac77a954dd2612fe5dc28914a3609e5e5191507a
jaison080 <93505829+jaison080@users.noreply.github.com>
2021-11-28 14:27:02 +0530

    Program 52
    Program to sort the numbers stored in array in ascending order.(bubble sort)

---
+++ Program52.c
@@ -0,0 +1,29 @@
+/*Program to sort the numbers stored in array in ascending order.(bubble sort)*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  	int array[100], n, c, d, swap;
+	system("clear");
+  	printf("Enter number of elements\n");
+  	scanf("%d", &n);
+	printf("Enter %d integers\n", n);
+	for (c = 0; c < n; c++)
+    		scanf("%d", &array[c]);
+	for (c = 0 ; c < n - 1; c++)
+  	{
+    		for (d = 0 ; d < n - c - 1; d++)
+    		{
+      			if (array[d] > array[d+1])
+      			{
+        			swap = array[d];
+        			array[d]   = array[d+1];
+        			array[d+1] = swap;
+      			}
+    		}
+  	}
+	printf("Sorted list in ascending order:\n");
+	for (c = 0; c < n; c++)
+     	printf("%d\n", array[c]);
+	return 0;
+}
